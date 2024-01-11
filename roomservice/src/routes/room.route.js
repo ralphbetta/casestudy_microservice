0fd9e8b1bee16d1f45b88339a58875a28e4bbf3e
Betta Raphael <betta@Bettas-MacBook-Pro-2.local>
2024-01-11 11:31:25 +0100

    complete romm service

--- roomservice/src/routes/room.route.js
+++ roomservice/src/routes/room.route.js
@@ -6,7 +6,7 @@ const router = require("express").Router();
 router.post("/room", TokenMiddleware.verify, controller.create);
 router.put("/room/:room_id", TokenMiddleware.verify, controller.updateRoom);
 router.post("/room/:room_id", TokenMiddleware.verify, controller.joinRoom);
-router.get("/participants", TokenMiddleware.verify, controller.participants);
+router.get("/room/:room_id/participants", TokenMiddleware.verify, controller.participants);
 
 
 
